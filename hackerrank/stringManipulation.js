b7872209e49db4b6e828a2e9683dce9981384d01
royanguiano <roya@tuborrow.com>
2017-09-08 17:07:13 -0700

    added hacker rank coding answers

--- hackerrank/stringManipulation.js
+++ hackerrank/stringManipulation.js
@@ -181,7 +181,7 @@ function main() {
 }
 
 
-console.log(twoStrings('hello', 'worldhel')); 
+//console.log(twoStrings('hello', 'worldhel')); 
 
 function twoStrings(s1, s2){
 
@@ -196,12 +196,213 @@ function twoStrings(s1, s2){
 
 
 
+console.log(checkforHackerRank('hereiamstackerrank')); 
+
+
+
+function checkforHackerRank(str){
+  let hr = 'hackerrank'; 
+
+  if(str.length < hr.length) return 'NO'; 
+
+  let j = 0; 
+  for(let i = 0; i < str.length; i++){
+    let char = hr.charAt(j); 
+    let shar = str.charAt(i); 
+    if(j < str.length && char == shar){
+      j++; 
+    }
+  }
+
+  return (j == hr.length ? 'YES': 'NO'); 
+}
 
 
 
 
+console.log(makingAnagrams('cde', 'abc'))
 
 
+function makingAnagrams(str1, str2){
+  let chars = []; 
+
+
+  for(let c1 of str1.split('')) { 
+    let c= chars[c1 - 97]++; 
+  }
+  for(let c2 of str2.split('')) { 
+    let cc = chars[c2 - 97]--; 
+  }
+
+  let count = 0; 
+  for(let i of chars) { 
+    let ii = i; 
+    count += Math.abs(i); }
+
+  console.log('count' , count);   
+
+}
+
+
+const funnyString = s => {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    let s1 = s[i].charCodeAt(0);
+    let s2 = s[i + 1].charCodeAt(0);
+    let r1 = s[s.length - i - 1].charCodeAt(0);
+    let r2 = s[s.length - i - 2].charCodeAt(0);
+    if (Math.abs(s1 - s2) !== Math.abs(r1 - r2)) return 'Not Funny';
+  }
+  return 'Funny';
+}
+
+
+
+//console.log(funnyString('bcxz')); 
+
+//console.log(gemstonesCalculate(['3', 'abcdde', 'baccd', 'eeabg'])); 
+
+function gemstonesCalculate(arr){
+  new Set(arr.slice(1)
+              .map(rock => rock.split(''))
+              .reduce(matchItems).size); 
+
+  function matchItems(previous, current){
+    function previousArray(element){
+      return previous.includes(element); 
+    }
+    
+    console.log(current.filter(previousArray));
+    return current;  
+  }
+}
+
+//console.log(funnyStringMatch(3, '0100101010')); 
+
+function funnyStringMatch(n, B){
+ let size = B.match(/010/g); 
+ return (size === null ? 0 : size.length); 
+}
+
+console.log(loveLetterProblem('abc'))
+
+function loveLetterProblem(s){
+
+}
+ 
+
+console.log(cesarCypher('middle-Outz', 2, 11)); 
+
+
+function cesarCypher(s, k, l){
+  let num = 0, ch, result = ''; 
+
+  for(let i = 0; i < l; i++){
+    num = 0; 
+    //let ch = s.charAt(i); 
+    let code = s.charCodeAt(i); 
+
+    if(code >= 65 && code <= 90){
+      num = code + k; 
+      if(num > 90){
+        num = 64 + num - 90; 
+      }
+      result += String.fromCharCode(num); 
+    }
+
+    if(code >= 97 && code <= 122){
+      num = code + k; 
+      if(num > 122){
+        num = 96 + num - 122;
+      }
+      result += String.fromCharCode(num);
+    }
+    if(s.charAt(i) === '-'){
+      result += '-'; 
+    }
+  }
+  return result;  
+}
+
+gameOfThrones('cdcdcdcdeeeef'); 
+
+function gameOfThrones(s){
+  let reg = s.toLowerCase(); 
+  let reverses = s.split('').reverse().join(''); 
+
+  return (reg == reverses ? 'YES' : 'NO'); 
+}
+
+
+console.log(getLetterFrequency('Wouldnt it make more sense for them to allow use of the beat for the 5 bucks or whatever the beat creater wants to charge but then also split sales.  The recording onthe phone is ok if you plug a mic into it.  Then if things are looking good (ie the song is popular) then they know investing in some studio time would make sense.  I dont know much about the music business but d know reggae/dancehall beats (that are referred to as riddims) tend to well on multiple songs  (if its good/popular riddim, multiple artists will use it and im sure sales are split somewhere along the way).﻿')); 
+
+console.log(strtotoken('Hi, welcome to CSC 220.It is your first assigment')); 
+
+function getLetterFrequency(S){
+  let counter = {}; 
+
+  for(let i = 0; i < S.length; i++){
+    if(counter[S[i]]){
+      counter[S[i]] += 1; 
+    } else {
+      counter[S[i]] = 1; 
+    }
+  }
+
+  for(let i in counter){
+    console.log(counter[i] , i); 
+  }
+}
+
+function strtotoken(S){
+  S = S.split(''); 
+
+  for(let i = 0; i <= S.length; i++){
+
+    if(i == S.length){
+      console.log(S.splice(0, i).join('')); 
+    }
+
+    if(S[i] == ' ' || S[i] == ',' || S[i] == '.'){
+      S.splice(i, 1); 
+      let char = i; 
+      console.log(S.splice(0, i ).join('')); 
+      i = 0; 
+    }
+  }
+}
+
+
+console.log(checkbits(6)); 
+
+function checkbits(n){
+  let bina = Number(n).toString(2); 
+  let counter = 0; 
+
+  for(let i in bina){
+    if(bina[i] == '1'){
+      counter++; 
+    } 
+  }
+  return counter; 
+}
+
+console.log(maxLength(10, 'beabeefeab')); 
+
+
+function maxLength(n, s){
+  let store = {}; 
+
+  for(let i = 0; i < s.length; i++){
+    if(!store[s[i]]){
+      store[s[i]] = 1; 
+    }
+  }
+
+  for(let i in store){
+    console.log(i); 
+  }
+
+}
 
 
 
